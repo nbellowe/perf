f8f909cb855a010f945f6915952a69bb700f3cfd
Nathan Bellowe <nbellowe@gmail.com>
2015-03-29 16:48:19 -0600

    Add some comments

--- FilterMain.cpp
+++ FilterMain.cpp
@@ -87,7 +87,7 @@ readFilter(string filename)
     }
 }
 
-
+;
 double
 applyFilter(struct Filter *_filter, cs1300bmp *input, cs1300bmp *output)
 {
@@ -96,25 +96,29 @@ applyFilter(struct Filter *_filter, cs1300bmp *input, cs1300bmp *output)
     cycStart = rdtscll();
 
     char *filter = _filter -> data;
-    /*#pragma omp parallel for
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            filter[i][j] =  filter -> get(i,j);
-        }
-    }*/
+    const short filter_divisor = _filter -> getDivisor(); //save clock cycles later on.
 
-    const short in_height = input -> height;
+    const short in_height = input -> height; //these are used in row_size and also for final output
     const short in_width  = input -> width;
-    const short row_size = in_height - 1;
-    const short col_size = in_width  - 1;
-    const short filter_divisor = _filter -> getDivisor();
 
+    const short row_size = in_height - 1; //we use this rather than recompute every time we are checking the condition of the next for loops.
+    const short col_size = in_width  - 1;
+
+    //note the order is best for pipelining.
     #pragma omp parallel for
     for(short plane = 2; plane >= 0 ; --plane ) { //best place for multicore performance benefits.
         for(short row = row_size, row_one = row+1, row_two=row+2; row > 0; --row, row_two=row_one-- ) {
-            for(short col = col_size, col_one = col_size+1, col_two = col_size+2; col > 0; --col, col_two=col_one-- ) {
-                short new_pixel;
-                
+            for(short col = col_size, col_one = col_size+1, col_two = col_size+2; col > 0; --col, col_two=col_one-- ) {o
+                //new_pixel will be initialized each time.
+                short new_pixel; //needs to be short even though working with chars, as char + char can overflow before division.
+
+                //Unrolled the two loops that are always 3 * 3 and made filter into a single dimensional array.
+                //I could speed this up farther, if I could figure out how to make a pointer to a triple dimensional array that is cached, e.g:
+                //unsigned char (*ptr)[8192][8192] = &(input -> color);
+                //ptr[plane][row][col]
+                //
+                //Also, I didn't want to have to recalclulate col+1, col+2 multiple times and row+1, row+2 so I cached col+1 up in the for loop
+                //initialization/increment statements. I did this weird: col_two=col_one--; is equivalent to col_two=col_one; --col_one
                 new_pixel  = input -> color[plane][row    ][col    ] * filter[0];
                 new_pixel += input -> color[plane][row    ][col_one] * filter[1];
                 new_pixel += input -> color[plane][row    ][col_two] * filter[2];
@@ -125,8 +129,11 @@ applyFilter(struct Filter *_filter, cs1300bmp *input, cs1300bmp *output)
                 new_pixel += input -> color[plane][row_two][col_one] * filter[7];
                 new_pixel += input -> color[plane][row_two][col_two] * filter[8];
 
+                // Now divide it.
                 new_pixel /= filter_divisor;
 
+                // I read somewhere conditionals are slightly faster than true if else... I'm not sure, but this is just a max/min function
+                // First checks if it is less than zero.
                 output -> color[plane][row][col] = new_pixel < 0   ? 0   : (new_pixel > 255 ? 255 : new_pixel);
             }
         }
